55b4d6fe333416d34a1a08529f54bd036dc66697
haguettaz <hugaguett@gmail.com>
2020-12-19 14:12:57 +0100

    activation functions

--- gechebnet/model/chebnet.py
+++ gechebnet/model/chebnet.py
@@ -65,8 +65,10 @@ class ChebNet(torch.nn.Module):
         # 2 convolutions + 1 spatial max pooling
         x = self.bn1(x)  # (B, C, V)
         x = self.conv1(x)  # (B, C, V)
+        x = F.relu(x)
         x = self.bn2(x)  # (B, C, V)
         x = self.conv2(x)  # (B, C, V)
+        x = F.relu(x)
         x = x.view(B, -1, self.nx3[0], self.nx2[0], self.nx1[0])  # (B, C, L, H, W)
         x = self.pooling(x, kernel_size=(1, 2, 2), stride=(1, 2, 2))  # (B, C, L, H', W')
         x = x.view(B, -1, self.nx3[1] * self.nx2[1] * self.nx1[1])  # (B, C, V)
@@ -74,8 +76,10 @@ class ChebNet(torch.nn.Module):
         # 2 convolutions + 1 spatial max pooling
         x = self.bn3(x)  # (B, C, V)
         x = self.conv3(x)  # (B, C, V)
+        x = F.relu(x)
         x = self.bn4(x)  # (B, C, V)
         x = self.conv4(x)  # (B, C, V)
+        x = F.relu(x)
         x = x.permute(0, 2, 1).contiguous().view(B, -1, self.nx3[1], self.nx2[1], self.nx1[1])  # (B, C, L, H, W)
         x = self.pooling(x, kernel_size=(1, 2, 2), stride=(1, 2, 2))  # (B, C, L, H', W')
         x = x.view(B, -1, self.nx3[2] * self.nx2[2] * self.nx1[2])  # (B, C, V)
@@ -83,8 +87,10 @@ class ChebNet(torch.nn.Module):
         # 2 convolutions + 1 global max pooling
         x = self.bn5(x)  # (B, C, V)
         x = self.conv5(x)  # (B, C, V)
+        x = F.relu(x)
         x = self.bn6(x)  # (B, C, V)
         x = self.conv6(x)  # (B, C, V)
+        x = F.relu(x)
         x = x.permute(0, 2, 1).contiguous().view(B, -1, self.nx3[2], self.nx2[2], self.nx1[2])  # (B, C, L, H, W)
         x = self.pooling(x, kernel_size=(self.nx3[2], self.nx2[2], self.nx1[2])).squeeze()  # (B, C)
 
