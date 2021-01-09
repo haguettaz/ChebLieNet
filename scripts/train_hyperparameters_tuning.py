3cda9b3dca11d42351c20d856aba89ba958e0af3
haguettaz <hugaguett@gmail.com>
2021-01-09 10:09:44 +0100

    test

--- scripts/train_hyperparameters_tuning.py
+++ scripts/train_hyperparameters_tuning.py
@@ -1,5 +1,6 @@
 import math
 import os
+import random
 
 import pykeops
 import torch
@@ -18,7 +19,7 @@ from torch.nn import NLLLoss
 from torch.nn.functional import nll_loss
 
 DATA_PATH = os.path.join(os.environ["TMPDIR"], "data")
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda")
 
 DATASET_NAME = "MNIST"  # STL10
 VAL_RATIO = 0.2
@@ -31,6 +32,8 @@ POOLING_SIZE = 2
 
 EPOCHS = 20
 OPTIMIZER = "adam"
+
+NUM_ITER = 10
 
 
 def build_sweep_config():
@@ -47,7 +50,7 @@ def build_sweep_config():
         "weight_sigma": {"distribution": "uniform", "min": 0.25, "max": 8.0},
         "weight_decay": {"distribution": "log_uniform", "min": math.log(1e-6), "max": math.log(1e-3)},
         "weight_kernel": {"distribution": "categorical", "values": ["cauchy", "gaussian", "laplacian"]},
-        "xi": {"distribution": "log_uniform", "min": 1e-2, "max": 1.0},
+        "xi": {"distribution": "log_uniform", "min": math.log(1e-2), "max": math.log(1.0)},
     }
 
     return sweep_config
@@ -85,7 +88,7 @@ def get_model(nx3, knn, eps, xi, weight_sigma, weight_kernel, K, pooling):
     )
     if graph_1.num_nodes > graph_1.num_edges:
         raise ValueError(f"An error occured during the computation of the graph")
-    wandb.log({f"graph_1_nodes": graph_1.num_nodes, f"graph_1_edges": graph_1.num_edges})
+    # wandb.log({f"graph_1_nodes": graph_1.num_nodes, f"graph_1_edges": graph_1.num_edges})
 
     graph_2 = HyperCubeGraph(
         grid_size=(NX1 // POOLING_SIZE, NX2 // POOLING_SIZE),
@@ -98,7 +101,7 @@ def get_model(nx3, knn, eps, xi, weight_sigma, weight_kernel, K, pooling):
     )
     if graph_2.num_nodes > graph_2.num_edges:
         raise ValueError(f"An error occured during the computation of the graph")
-    wandb.log({f"graph_2_nodes": graph_2.num_nodes, f"graph_2_edges": graph_2.num_edges})
+    # wandb.log({f"graph_2_nodes": graph_2.num_nodes, f"graph_2_edges": graph_2.num_edges})
 
     graph_3 = HyperCubeGraph(
         grid_size=(NX1 // POOLING_SIZE // POOLING_SIZE, NX2 // POOLING_SIZE // POOLING_SIZE),
@@ -111,7 +114,7 @@ def get_model(nx3, knn, eps, xi, weight_sigma, weight_kernel, K, pooling):
     )
     if graph_3.num_nodes > graph_3.num_edges:
         raise ValueError(f"An error occured during the computation of the graph")
-    wandb.log({f"graph_3_nodes": graph_3.num_nodes, f"graph_3_edges": graph_3.num_edges})
+    # wandb.log({f"graph_3_nodes": graph_3.num_nodes, f"graph_3_edges": graph_3.num_edges})
 
     model = ChebNet(
         (graph_1, graph_2, graph_3),
@@ -123,60 +126,123 @@ def get_model(nx3, knn, eps, xi, weight_sigma, weight_kernel, K, pooling):
         pooling=pooling,
     )
 
-    wandb.log({"capacity": model.capacity})
+    # wandb.log({"capacity": model.capacity})
 
     return model.to(DEVICE)
 
 
-def train(config=None):
-    # Initialize a new wandb run
-    with wandb.init(config=config):
-        config = wandb.config
+# def train(config=None):
+#     # Initialize a new wandb run
+#     with wandb.init(config=config):
+#         config = wandb.config
 
-        # Model and optimizer
-        model = get_model(
-            config.nx3,
-            config.knn,
-            config.eps,
-            config.xi,
-            config.weight_sigma,
-            config.weight_kernel,
-            config.K,
-            config.pooling,
-        )
+#         # Model and optimizer
+#         model = get_model(
+#             config.nx3,
+#             config.knn,
+#             config.eps,
+#             config.xi,
+#             config.weight_sigma,
+#             config.weight_kernel,
+#             config.K,
+#             config.pooling,
+#         )
 
-        optimizer = get_optimizer(model, OPTIMIZER, config.learning_rate, config.weight_decay)
-        loss_fn = nll_loss
+#         optimizer = get_optimizer(model, OPTIMIZER, config.learning_rate, config.weight_decay)
+#         loss_fn = nll_loss
 
-        # Trainer and evaluator(s) engines
-        trainer = create_supervised_trainer(
-            L=config.nx3,
-            model=model,
-            optimizer=optimizer,
-            loss_fn=loss_fn,
-            device=DEVICE,
-            prepare_batch=prepare_batch,
-        )
-        ProgressBar(persist=False, desc="Training").attach(trainer)
+#         # Trainer and evaluator(s) engines
+#         trainer = create_supervised_trainer(
+#             L=config.nx3,
+#             model=model,
+#             optimizer=optimizer,
+#             loss_fn=loss_fn,
+#             device=DEVICE,
+#             prepare_batch=prepare_batch,
+#         )
+#         ProgressBar(persist=False, desc="Training").attach(trainer)
 
-        metrics = {"validation_accuracy": Accuracy(), "validation_loss": Loss(loss_fn)}
+#         metrics = {"validation_accuracy": Accuracy(), "validation_loss": Loss(loss_fn)}
 
-        evaluator = create_supervised_evaluator(
-            L=config.nx3, model=model, metrics=metrics, device=DEVICE, prepare_batch=prepare_batch
-        )
-        ProgressBar(persist=False, desc="Evaluation").attach(evaluator)
+#         evaluator = create_supervised_evaluator(
+#             L=config.nx3, model=model, metrics=metrics, device=DEVICE, prepare_batch=prepare_batch
+#         )
+#         ProgressBar(persist=False, desc="Evaluation").attach(evaluator)
 
-        train_loader, val_loader = get_train_val_data_loaders(
-            DATASET_NAME, batch_size=config.batch_size, val_ratio=VAL_RATIO, data_path=DATA_PATH
-        )
+#         train_loader, val_loader = get_train_val_data_loaders(
+#             DATASET_NAME, batch_size=config.batch_size, val_ratio=VAL_RATIO, data_path=DATA_PATH
+#         )
 
-        # Performance tracking with wandb
-        trainer.add_event_handler(Events.EPOCH_COMPLETED, wandb_log, evaluator, val_loader)
+#         # Performance tracking with wandb
+#         trainer.add_event_handler(Events.EPOCH_COMPLETED, wandb_log, evaluator, val_loader)
 
-        trainer.run(train_loader, max_epochs=EPOCHS)
+#         trainer.run(train_loader, max_epochs=EPOCHS)
+
+
+def train():
+
+    # graph
+    nx3 = random.randint(3, 12)
+    eps = math.exp(random.uniform(math.log(0.1), math.log(1.0)))
+    xi = math.exp(random.uniform(math.log(1e-2), math.log(1.0)))
+    knn = random.choice([2, 4, 8, 16, 32])
+    weight_kernel = random.choice(["cauchy", "gaussian", "laplacian"])
+    weight_sigma = math.exp(random.uniform(math.log(0.25), math.log(10)))
+
+    # network
+    K = random.choice([2, 4, 8, 16, 32, 64])
+    pooling = random.choice(["max", "avg"])
+
+    # training
+    batch_size = random.choice([8, 16, 32, 64, 128, 256])
+    learning_rate = math.exp(random.uniform(math.log(1e-5), math.log(0.1)))
+    weight_decay = math.exp(random.uniform(math.log(1e-7), math.log(1e-2)))
+
+    # Model and optimizer
+    model = get_model(
+        nx3,
+        knn,
+        eps,
+        xi,
+        weight_sigma,
+        weight_kernel,
+        K,
+        pooling,
+    )
+
+    optimizer = get_optimizer(model, OPTIMIZER, learning_rate, weight_decay)
+    loss_fn = nll_loss
+
+    # Trainer and evaluator(s) engines
+    trainer = create_supervised_trainer(
+        L=nx3,
+        model=model,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        device=DEVICE,
+        prepare_batch=prepare_batch,
+    )
+    ProgressBar(persist=False, desc="Training").attach(trainer)
+
+    metrics = {"validation_accuracy": Accuracy(), "validation_loss": Loss(loss_fn)}
+
+    evaluator = create_supervised_evaluator(
+        L=nx3, model=model, metrics=metrics, device=DEVICE, prepare_batch=prepare_batch
+    )
+    ProgressBar(persist=False, desc="Evaluation").attach(evaluator)
+
+    train_loader, val_loader = get_train_val_data_loaders(
+        DATASET_NAME, batch_size=batch_size, val_ratio=VAL_RATIO, data_path=DATA_PATH
+    )
+
+    # Performance tracking with wandb
+    # trainer.add_event_handler(Events.EPOCH_COMPLETED, wandb_log, evaluator, val_loader)
+
+    # trainer.run(train_loader, max_epochs=EPOCHS)
 
 
 if __name__ == "__main__":
-    sweep_config = build_sweep_config()
-    sweep_id = wandb.sweep(sweep_config, project="gechebnet")
-    wandb.agent(sweep_id, train, count=50)
+    # sweep_config = build_sweep_config()
+    # sweep_id = wandb.sweep(sweep_config, project="gechebnet")
+    # wandb.agent(sweep_id, train, count=50)
+    train()
