7e47ce2a4f7ea1a9f43d0a0f12418ae8107999b4
haguettaz <hugaguett@gmail.com>
2021-01-09 22:36:35 +0100

    minor fix

--- scripts/hpo.py
+++ scripts/hpo.py
@@ -154,7 +154,7 @@ def train(config=None):
         # Performance tracking with wandb
         trainer.add_event_handler(Events.EPOCH_COMPLETED, wandb_log, evaluator, val_loader)
 
-        trainer.run(train_loader, max_epochs=EPOCHS)
+        # trainer.run(train_loader, max_epochs=EPOCHS)
 
 
 if __name__ == "__main__":
