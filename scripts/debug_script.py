787b843b52c46be4a449f9ffd707a21f4c0fa7e6
haguettaz <hugaguett@gmail.com>
2021-01-10 00:37:48 +0100

    minor fix

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -12,16 +12,9 @@ from gechebnet.model.chebnet import GEChebNet
 from gechebnet.model.optimizer import get_optimizer
 from ignite.metrics import Accuracy, Loss
 from torch.nn.functional import nll_loss
+from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.datasets import MNIST, STL10
-from torchvision.transforms import (
-    Compose,
-    Normalize,
-    RandomHorizontalFlip,
-    RandomRotation,
-    RandomVerticalFlip,
-    ToTensor,
-)
 
 NX1, NX2 = (28, 28)
 DATASET = "mnist"
