5a703899bf14f9342cd6ba83d0de02e99e131bd0
haguettaz <hugaguett@gmail.com>
2021-01-10 00:33:03 +0100

    minor fix

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -12,7 +12,7 @@ from gechebnet.model.chebnet import GEChebNet
 from gechebnet.model.optimizer import get_optimizer
 from ignite.metrics import Accuracy, Loss
 from torch.nn.functional import nll_loss
-from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.datasets import MNIST, STL10
 from torchvision.transforms import (
     Compose,
